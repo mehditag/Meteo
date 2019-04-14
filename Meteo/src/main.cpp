a3daef783469b7a2f9a031fb388c5a3c7b80468b
mehditag <48520059+mehditag@users.noreply.github.com>
2019-04-14 23:16:11 +0200

    Add Timer

--- Meteo/src/main.cpp
+++ Meteo/src/main.cpp
@@ -1,9 +1,18 @@
+#include "Objets.h"
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext> 
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-    QQmlApplicationEngine engine(QUrl(QStringLiteral("qrc:/qml/main.qml")));
+    
+    
+    Objets capteur;
+    
+	QQmlApplicationEngine engine;
+	engine.rootContext()->setContextProperty("capteur", &capteur);
+	engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
+    
     return app.exec();
 }
