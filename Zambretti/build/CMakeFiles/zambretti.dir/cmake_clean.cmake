ecc551e471cb6ae0b70e8c6214d1c99731fe8d70
axilixa <48520038+axilixa@users.noreply.github.com>
2019-04-15 13:57:40 +0200

    ajout_calcul_zambretti
    calcul de zambretti, avec choix de la pression via bucle for et calcul de la tendance en fonction de la valeur précédente de pression.

---
+++ Zambretti/build/CMakeFiles/zambretti.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zambretti.dir/main.cpp.o"
+  "zambretti.pdb"
+  "zambretti"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zambretti.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Zambretti/main.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(int argc, char **argv) {
+ // int tendance = -5;
+    int tendance;
+  //int pression = 1030;
+    int altitude = 151.5;
+    int zambretti_coefficient;
+    int stock_pression;
+    
+    
+    
+    
+    for   (int pression=900; pression <1051; pression=pression+10){
+        
+    
+    
+    int  pression_calc = pression/ pow(1-(0.0065*altitude/288.15),5.255);
+    
+    tendance = pression_calc - stock_pression;
+    cout << "tendance : "<< tendance<<endl;
+    
+    
+    cout << "pression au niveau de la mer" << pression_calc << endl;
+    
+    if ((tendance <-2)){
+ 
+        zambretti_coefficient = 130-(10*pression_calc/81);
+    //  cout<< "tendance descendante" <<endl;
+        
+  } else if (tendance >2) {
+            
+            zambretti_coefficient = 179-((20*pression_calc)/129);
+       //   cout<< zambretti_coefficient << endl;
+            
+            
+    }else {            
+              zambretti_coefficient = 147-((50*pression_calc)/376);
+     //     cout<< zambretti_coefficient << endl;
+    
+            
+            
+        }
+        
+                
+              switch (zambretti_coefficient){
+                  
+                  case 1: cout << "Il fait beau"<< endl; break;
+                  case 2: cout << "Il fait beau 2"<< endl; break;
+                  case 3: cout << "Il fait beau 3"<< endl; break;
+                  case 4: cout << "pluie fine" << endl; break;
+                  case 5: cout << "incertain"<< endl; break;
+                  case 6: cout << "incertain"<< endl; break;
+                  case 7:cout << "pluie" << endl; break;
+                  case 8: cout << "averses" << endl; break;
+                  case 9: cout << "averses" << endl; break;                            
+                  case 10:cout << "Il fait beau"<< endl; break;
+                  case 11:cout << "Il fait beau 2"<< endl; break;
+                  case 12:cout << "Il fait beau 3"<< endl; break;
+                  case 13:cout << "soleil + nuages" << endl; break;
+                  case 14:cout << "pluie fine" << endl; break;
+                  case 15:cout << "incertain"<< endl; break;
+                  case 16:cout << "incertain"<< endl; break;
+                  case 17:cout << "pluie" << endl; break;
+                  case 18:cout << "averses" << endl; break;
+                  case 19:cout << "averses" << endl; break;
+                  case 20:cout << "Il fait beau"<< endl; break;
+                  case 21:cout << "Il fait beau 2"<< endl; break;
+                  case 22:cout << "Il fait beau 3"<< endl; break;
+                  case 23:cout << "soleil + nuages" << endl; break;
+                  case 24:cout << "pluie fine" << endl; break;
+                  case 25:cout << "incertain"<< endl; break;
+                  case 26:cout << "incertain"<< endl; break;
+                  case 27:cout << "pluie" << endl; break;
+                  case 28:cout << "averses" << endl; break;
+                  case 29:cout << "averses" << endl; break;
+                  case 30:cout << "incertain"<< endl; break;
+                  case 31:cout << "tempête" << endl; break;
+                  case 32:cout << "tempete" << endl; break;
+                  
+                default: cout << "are you serious"<< endl; break;
+              }
+              
+              
+                stock_pression = pression_calc;
+    }
+    
+    return 0;
+}
+ 
